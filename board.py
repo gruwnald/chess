3861493ff1702ab7d0ac34201a90dc443ae47954
Jerzy Grunwald <jurek@egrunwald.pl>
2024-02-19 16:37:22 +0100

    Added check status check

--- board.py
+++ board.py
@@ -1,8 +1,29 @@
 from pieces import *
-
+from copy import deepcopy
 class Board:
     def __init__(self):
         self.board = [[None] * 8 for _ in range(8)]
+
+        self.white_pieces = [
+            Rook('white', 7, 0), Rook('white', 7, 7),
+            Knight('white', 7, 1), Knight('white', 7, 6),
+            Bishop('white', 7, 2), Bishop('white', 7, 5),
+            Queen('white', 7, 3), King('white', 7, 4),
+            Pawn('white', 6, 0), Pawn('white', 6, 1),
+            Pawn('white', 6, 2), Pawn('white', 6, 3),
+            Pawn('white', 6, 4), Pawn('white', 6, 5),
+            Pawn('white', 6, 6), Pawn('white', 6, 7)
+        ]
+        self.black_pieces = [
+            Rook('black', 0, 0), Rook('black', 0, 7),
+            Knight('black', 0, 1), Knight('black', 0, 6),      
+            Bishop('black', 0, 2), Bishop('black', 0, 5),
+            Queen('black', 0, 3), King('black', 0, 4),
+            Pawn('black', 1, 0), Pawn('black', 1, 1),
+            Pawn('black', 1, 2), Pawn('black', 1, 3),
+            Pawn('black', 1, 4), Pawn('black', 1, 5),
+            Pawn('black', 1, 6), Pawn('black', 1, 7)
+        ]
 
     def get_piece(self, row, col):
         return self.board[row][col]
@@ -11,27 +32,38 @@ class Board:
         self.board[row][col] = piece
     
     def starting_board(self):
-        self.set_piece(0, 0, Rook('black', 0, 0))
-        self.set_piece(0, 7, Rook('black', 0, 7))
-        self.set_piece(7, 0, Rook('white', 7, 0))
-        self.set_piece(7, 7, Rook('white', 7, 7))
-        self.set_piece(0, 1, Knight('black', 0, 1))
-        self.set_piece(0, 6, Knight('black', 0, 6))
-        self.set_piece(7, 1, Knight('white', 7, 1))
-        self.set_piece(7, 6, Knight('white', 7, 6))
-        self.set_piece(0, 2, Bishop('black', 0, 2))
-        self.set_piece(0, 5, Bishop('black', 0, 5))
-        self.set_piece(7, 2, Bishop('white', 7, 2))
-        self.set_piece(7, 5, Bishop('white', 7, 5))
-        self.set_piece(0, 3, Queen('black', 0, 3))
-        self.set_piece(7, 3, Queen('white', 7, 3))
-        self.set_piece(0, 4, King('black', 0, 4))
-        self.set_piece(7, 4, King('white', 7, 4))
-        for i in range(8):
-            self.set_piece(1, i, Pawn('black', 1, i))
-            self.set_piece(6, i, Pawn('white', 6, i))
-    
-    def legal_moves(self, start_row, start_col, end_row, end_col):
+        for piece in self.white_pieces:
+            self.set_piece(piece.row, piece.col, piece)
+        for piece in self.black_pieces:
+            self.set_piece(piece.row, piece.col, piece)
+
+
+    def in_check(self, color):
+        if color == "white":
+            # Find the white king
+            for piece in self.white_pieces:
+                if isinstance(piece, King):
+                    white_king = piece
+                    break
+            
+            for piece in self.black_pieces:
+                if piece.captures()[white_king.row][white_king.col] == 1:
+                    return True
+            return False
+        else:
+            # Find the black king
+            for piece in self.black_pieces:
+                if isinstance(piece, King):
+                    black_king = piece
+                    break
+            
+            for piece in self.white_pieces:
+                if piece.captures()[black_king.row][black_king.col] == 1:
+                    return True
+            return False
+
+
+    def is_valid_move(self, start_row, start_col, end_row, end_col):
         piece = self.get_piece(start_row, start_col)
 
         if piece is None:
@@ -43,6 +75,12 @@ class Board:
             potential_moves = piece.captures()
         else:
             potential_moves = piece.moves()
+
+        if self.in_check(piece.color):
+            temp_board = deepcopy(self)
+            temp_board.move_piece(start_row, start_col, end_row, end_col)
+            if temp_board.in_check(piece.color):
+                return False
 
         if isinstance(piece, Knight) or isinstance(piece, King) or isinstance(piece, Pawn):
             return potential_moves[end_row][end_col] == 1
@@ -135,10 +173,19 @@ class Board:
                                 return False
             return True
 
-    
+
     def move_piece(self, start_row, start_col, end_row, end_col):
         piece = self.get_piece(start_row, start_col)
-        if self.legal_moves(start_row, start_col, end_row, end_col):
+
+        if self.is_valid_move(start_row, start_col, end_row, end_col):
+            #Remove captured piece from list
+            if self.get_piece(end_row, end_col) is not None:
+                if self.get_piece(end_row, end_col).color == 'white':
+                    self.white_pieces.remove(self.get_piece(end_row, end_col))
+                else:
+                    self.black_pieces.remove(self.get_piece(end_row, end_col))
+
+            #Move piece
             self.set_piece(end_row, end_col, piece)
             self.set_piece(start_row, start_col, None)
             piece.row = end_row
--- game.py
+++ game.py
@@ -6,13 +6,36 @@ class Game:
         self.board = Board()
         self.board.starting_board()
         self.white_to_move = True
+        self.white_in_check = False
+        self.black_in_check = False
+    
+    
+    def make_move(self, start_row, start_col, end_row, end_col):
+        piece = self.board.get_piece(start_row, start_col)
+        if piece is None:
+            return False
+        if piece.color == 'black' and self.white_to_move:
+            return False
+        if piece.color == 'white' and not self.white_to_move:
+            return False
+        if not self.board.is_valid_move(start_row, start_col, end_row, end_col):
+            return False
+        self.board.move_piece(start_row, start_col, end_row, end_col)
+        self.white_to_move = not self.white_to_move
+        # Update in_check status
+        self.white_in_check = self.board.in_check("white")
+        self.black_in_check = self.board.in_check("black")
+        return True
 
 if __name__=="__main__":
+
+
     g = Game()
-    g.board.move_piece(6, 4, 4, 4)
-    g.board.move_piece(1, 4, 3, 4)
-    g.board.move_piece(6, 3, 4, 3)
-    g.board.move_piece(1, 3, 3, 3)
-    g.board.move_piece(4, 4, 3, 3)
-    g.board.move_piece(3, 3, 4, 4)
+    g.make_move(6, 4, 4, 4)
+    g.make_move(1, 4, 3, 4)
+    g.make_move(6, 3, 4, 3)
     g.board.print_board()
+    #g.make_move(3, 4, 4, 3)
+    # g.board.print_board()
+    # for piece in g.board.white_pieces:
+    #     print(piece, piece.color, piece.row, piece.col)
